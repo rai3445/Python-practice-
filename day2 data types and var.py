1833f74dda5eb4879c9ae66f3a40f63c4eff9075
Gobind Rai <gr4153632@gmail.com>
2026-07-30 01:15:11 +0530

    Day 2: Variables and Data Types

---
+++ day2 data types and var.py
@@ -0,0 +1,12 @@
+age = 23
+height = 5.2
+name = "Gobind"
+is_student = True
+print("Age:", age)
+print("Height:", height)
+print("Name:", name)
+print("Is Student:", is_student)
+print("Data type of age:", type(age))
+print("Data type of height:", type(height))
+print("Data type of name:", type(name))
+print("Data type of is_student:", type(is_student))
